21b5245b0c6356727f08a67f104658e5fbaabe8d
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-10 11:05:47 -0400

    formated and gaved commit for the codes

--- main.ts
+++ main.ts
@@ -5,49 +5,7 @@
  * This program ligths the trafic lights 
 */
 
+// idenfifying the trafic light 
+let traficlLigth: neopixel.Strip = null
 //idenfifying the trafic light 
 let traficlLigth: neopixel.Strip = null
-
-//seting the screen and setup
-basic.clearScreen()
-traficlLigth=neopixel.create(DigitalPin.P16, 4,NeoPixelMode.RGB)
-traficlLigth.setPixelColor(0,neopixel.colors(NeoPixelColors.Black))
-traficlLigth.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-traficlLigth.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-traficlLigth.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-traficlLigth.show()
-basic.showIcon(IconNames.Happy)
-let showLamps=true;
-//Setting up the our trafic lights and truing on
-input.onButtonPressed(Button.A,function(){
-    basic.showIcon(IconNames.Happy)
-    showLamps=true;
-    while (showLamps)
-    {   if(showLamps) traficlLigth.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-        traficlLigth.show()
-        basic.pause(1000)
-        traficlLigth.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        basic.pause(1000)
-        if (showLamps) traficlLigth.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-        traficlLigth.show()
-        basic.pause(1000)
-        traficlLigth.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        basic.pause(1000)
-        if (showLamps) traficlLigth.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-        traficlLigth.show()
-        basic.pause(1000)
-        traficlLigth.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    }
-})
-
-//Trun off
-input.onButtonPressed(Button.B,function(){
-    showLamps=false;
-    traficlLigth.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    traficlLigth.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    traficlLigth.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    traficlLigth.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    traficlLigth.show()
-    basic.showIcon(IconNames.SmallDiamond)
-})
-
