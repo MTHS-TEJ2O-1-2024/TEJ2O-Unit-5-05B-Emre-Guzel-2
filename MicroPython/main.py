20825db584fb778dc062681d56f73acc7fc557cb
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-10 17:28:46 +0000

    setup the trafic light

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,14 @@ This module is a Micro:bit MicroPython program  ligths the trafic lights
 """
 
 from microbit import *
-import neopixel
 
+# Setting the neopixle
+trafic_Light = neopixel.NeoPixel(pin16, 4)
+
+# Setting the screen and colors
+trafic_Light[0] = (0, 0, 0)
+trafic_Light[1] = (0, 0, 0)
+trafic_Light[2] = (0, 0, 0)
 display.clear()
+display.show(Image.HAPPY)
+trafic_Light.show()
