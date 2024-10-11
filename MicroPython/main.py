916004afca00dde456e29f6f313bc5519b1cf9fa
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-11 01:08:23 +0000

    added the trafic light

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,14 +5,50 @@ This module is a Micro:bit MicroPython program  ligths the trafic lights
 """
 
 from microbit import *
+import neopixel
 
 # Setting the neopixle
-trafic_Light = neopixel.NeoPixel(pin16, 4)
+trafic_Light = neopixel.NeoPixel(pin16, 3)
 
 # Setting the screen and colors
 trafic_Light[0] = (0, 0, 0)
 trafic_Light[1] = (0, 0, 0)
 trafic_Light[2] = (0, 0, 0)
 display.clear()
-display.show(Image.HAPPY)
 trafic_Light.show()
+
+while True:
+    # Setting up our RGB LED's
+    if button_a.is_pressed():
+        display.show(Image.SQUARE)
+        # Red
+        trafic_Light[0] = (255, 0, 0)
+        trafic_Light.show()
+        sleep(1000)
+        # Turining off the Red LED
+        trafic_Light[0] = (0, 0, 0)
+        sleep(1000)
+        trafic_Light.show()
+
+        # Yellow LED
+        trafic_Light[1] = (255, 234, 0)
+        trafic_Light.show()
+        sleep(1000)
+        trafic_Light[1] = (0, 0, 0)
+        sleep(1000)
+        trafic_Light.show()
+        # Green
+        trafic_Light[2] = (255, 128, 0)
+        trafic_Light.show()
+        sleep(1000)
+        trafic_Light[2] = (0, 0, 0)
+        sleep(1000)
+        trafic_Light.show()
+
+        # Turn off
+    if button_b.is_pressed:
+        trafic_Light[0] = (0, 0, 0)
+        trafic_Light[1] = (0, 0, 0)
+        trafic_Light[2] = (0, 0, 0)
+        display.show(Image.DIAMOND)
+        trafic_Light.show()
